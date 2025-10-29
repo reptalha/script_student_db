43ed183df86e62662cae1bcf910eebf451964ea7
a97424mc <mohammad.chaudhry-2@student.manchester.ac.uk>
2025-10-29 10:41:55 +0000

    3/5

--- periodic_table/periodic_table.sql
+++ periodic_table/periodic_table.sql
@@ -117,3 +117,65 @@ ALTER TABLE ONLY public.types ALTER COLUMN type_id SET DEFAULT nextval('public.t
 --
 -- Data for Name: elements; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
+
+
+
+INSERT INTO public.elements VALUES (1, 'H', 'Hydrogen');
+INSERT INTO public.elements VALUES (2, 'He', 'Helium');
+INSERT INTO public.elements VALUES (3, 'Li', 'Lithium');
+INSERT INTO public.elements VALUES (4, 'Be', 'Beryllium');
+INSERT INTO public.elements VALUES (5, 'B', 'Boron');
+INSERT INTO public.elements VALUES (6, 'C', 'Carbon');
+INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
+INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
+INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
+INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
+
+
+--
+-- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.properties VALUES (1, 1.008, -259.1, -252.9, 1);
+INSERT INTO public.properties VALUES (2, 4.0026, -272.2, -269, 1);
+INSERT INTO public.properties VALUES (3, 6.94, 180.54, 1342, 1);
+INSERT INTO public.properties VALUES (4, 9.0122, 1287, 2470, 1);
+INSERT INTO public.properties VALUES (5, 10.81, 2075, 4000, 1);
+INSERT INTO public.properties VALUES (6, 12.011, 3550, 4027, 1);
+INSERT INTO public.properties VALUES (7, 14.007, -210.1, -195.8, 1);
+INSERT INTO public.properties VALUES (8, 15.999, -218, -183, 1);
+INSERT INTO public.properties VALUES (9, 18.998, -220, -188.1, 2);
+INSERT INTO public.properties VALUES (10, 20.18, -248.6, -246.1, 2);
+
+
+--
+-- Data for Name: types; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.types VALUES (1, 'metal');
+INSERT INTO public.types VALUES (2, 'nonmetal');
+INSERT INTO public.types VALUES (3, 'metalloid');
+
+
+--
+-- Name: types_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.types_type_id_seq', 3, true);
+
+
+--
+-- Name: elements elements_atomic_number_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.elements
+    ADD CONSTRAINT elements_atomic_number_key UNIQUE (atomic_number);
+
+
+--
+-- Name: elements elements_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.elements
+    ADD CONSTRAINT elements_pkey PRIMARY KEY (atomic_number);
+
